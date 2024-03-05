f206bd54d7fc69a001ddb41401bfb57f81e4bf2f
Hugo Leonardo <huleonardo19@gmail.com>
2024-03-05 19:54:58 -0300

    feat: creates setup archive for sequelize

---
+++ database/sequelize.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+import pg from 'pg'
+import User from "./models/user.model";
+
+export const sequelize = new Sequelize(process.env.POSTGRES_URL_NON_POOLING ?? '', {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: 2,
+    min: 0,
+    idle: 0,
+    acquire: 3000,
+  },
+})
