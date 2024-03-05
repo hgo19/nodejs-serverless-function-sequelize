e7bdf5a6dddd3ae35228582cb4f1d9cb24ed26d3
Hugo Leonardo <huleonardo19@gmail.com>
2024-03-05 20:00:29 -0300

    chore: add types/pg

--- database/sequelize.ts
+++ database/sequelize.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-import pg from 'pg'
+import * as pg from 'pg'
 import User from "./models/user.model";
 
 export const sequelize = new Sequelize(process.env.POSTGRES_URL_NON_POOLING ?? '', {
