ff4b7175af9437ec2ec1d8c4999ab6b7a4d63e9e
Hugo Leonardo <huleonardo19@gmail.com>
2024-03-06 19:22:56 -0300

    feat: add req on response

--- api/test.ts
+++ api/test.ts
@@ -5,7 +5,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   const token = req.headers.Authorization;
   console.log(req.headers)
 
-  return res.status(401).json({ message: 'Unauthorized', headers: req.headers });
+  return res.status(401).json({ message: 'Unauthorized', headers: req.headers, req: req });
 }
 
 
